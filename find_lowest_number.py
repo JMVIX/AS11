ca39ce2186e7db6fb505b6bc957a07bc78feadf8
JMVIX <43823461+JMVIX@users.noreply.github.com>
2025-04-07 19:32:44 -0700

    Add files via upload

---
+++ find_lowest_number.py
@@ -0,0 +1,24 @@
+import sys
+
+input_filename = sys.argv[1]
+output_filename = sys.argv[2]
+
+number_found = False
+
+with open(input_filename, 'r') as input_file:
+    for line in input_file:
+        if number_found == False:
+            try:
+                lowest_number = float(line)
+                number_found = True
+            except ValueError:
+                break
+        else:
+            if float(line) < lowest_number:
+                lowest_number = float(line)
+
+with open(output_filename, 'w') as output_file:
+    if number_found:
+        output_file.write(str(lowest_number) + "\n")
+    else:
+        output_file.write("No numbers found in file\n")
